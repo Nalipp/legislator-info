66d801c542bf910bcd3bb8fd511c024f690aa4e0
Nalipp <nateplipp@gmail.com>
2016-05-26 17:18:41 +0900

    write letters to individual files

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -8,8 +8,30 @@ def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5,'0')[0..4]
 end
 
+def clean_phone_number(home_phone)
+  home_phone.gsub!(/[^0-9]/, "")
+
+  if home_phone.length < 10 || home_phone.length > 11
+    nil
+  elsif home_phone.length == 11 && home_phone[0] == '1'
+    home_phone[1..-1]
+  else
+    home_phone
+  end
+end
+
 def legislators_by_zipcode(zipcode)
   legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
+end
+
+def save_thank_you_letters(zipcode)
+  Dir.mkdir("output") unless Dir.exists? "output"
+
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename, 'w') do |file|
+    file.puts form_letter
+  end
 end
 
 puts "EventManager initialized."
@@ -20,12 +42,13 @@ template_letter = File.read "../form_letter.erb"
 erb_template = ERB.new template_letter
 
 @contents.each do |row|
+  id = row[0]
   name = row[:first_name]
 
   zipcode = clean_zipcode(row[:zipcode])
+  home_phone = clean_phone_number(row[:homephone])
 
   legislators = legislators_by_zipcode(zipcode)
 
   form_letter = erb_template.result(binding)
-  puts form_letter
 end
